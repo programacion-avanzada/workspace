88d18ed37242506173077f1f85493c98e57ac420
Frederick <federicogasior@gmail.com>
2020-05-12 16:45:26 -0300

    PR review

---
+++ oia/computando_patentes/src/computandoPatentes/Conversor2016.java
@@ -0,0 +1,37 @@
+package computandoPatentes;
+
+public class Conversor2016 extends Conversor {
+	@Override
+	protected long getNumero(Patente patente) {
+		char[] simbolos = patente.valor.toCharArray();
+		long numerico = 0;
+		numerico += (simbolos[6] + CHAR_A_VALOR_LETRA);
+		numerico += (simbolos[5] + CHAR_A_VALOR_LETRA) * LETRAS;
+		numerico += (simbolos[4] + CHAR_A_VALOR_NUMERO) * LETRAS2;
+		numerico += (simbolos[3] + CHAR_A_VALOR_NUMERO) * LETRAS2 * NUMEROS;
+		numerico += (simbolos[2] + CHAR_A_VALOR_NUMERO) * LETRAS2 * NUMEROS2;
+		numerico += (simbolos[1] + CHAR_A_VALOR_LETRA) * LETRAS2 * NUMEROS3;
+		numerico += (simbolos[0] + CHAR_A_VALOR_LETRA) * LETRAS3 * NUMEROS3;
+		return numerico;
+	}
+
+	@Override
+	protected String getPatente(long numerico) {
+		char[] simbolos = new char[7];
+		simbolos[0] = (char) (numerico / (LETRAS3 * NUMEROS3) + VALOR_LETRA_A_CHAR);
+		numerico %= LETRAS3 * NUMEROS3;
+		simbolos[1] = (char) (numerico / (LETRAS2 * NUMEROS3) + VALOR_LETRA_A_CHAR);
+		numerico %= LETRAS2 * NUMEROS3;
+		simbolos[2] = (char) (numerico / (LETRAS2 * NUMEROS2) + VALOR_NUMERO_A_CHAR);
+		numerico %= LETRAS2 * NUMEROS2;
+		simbolos[3] = (char) (numerico / (LETRAS2 * NUMEROS) + VALOR_NUMERO_A_CHAR);
+		numerico %= LETRAS2 * NUMEROS;
+		simbolos[4] = (char) (numerico / LETRAS2 + VALOR_NUMERO_A_CHAR);
+		numerico %= LETRAS2;
+		simbolos[5] = (char) (numerico / LETRAS + VALOR_LETRA_A_CHAR);
+		numerico %= LETRAS;
+		simbolos[6] = (char) (numerico + VALOR_LETRA_A_CHAR);
+
+		return new String(simbolos);
+	}
+}
--- oia/computando_patentes/src/tests/PatenteTests.java
+++ oia/computando_patentes/src/tests/PatenteTests.java
@@ -3,34 +3,34 @@ package tests;
 import org.junit.Assert;
 import org.junit.Test;
 
-import computando_patentes.Patente;
+import computandoPatentes.Patente;
 
 public class PatenteTests {
 	@Test
 	public void testPatente1995() {
-		Patente p;
+		Patente patente;
 
-		p = new Patente("AAA001");
-		Assert.assertEquals("AAB101", p.getPatenteSiguiente(1100));
+		patente = new Patente("AAA001");
+		Assert.assertEquals("AAB101", patente.getPatenteSiguiente(1100));
 
-		p = new Patente("AZZ999");
-		Assert.assertEquals("BAA000", p.getPatenteSiguiente(1));
+		patente = new Patente("AZZ999");
+		Assert.assertEquals("BAA000", patente.getPatenteSiguiente(1));
 
-		p = new Patente("BBB222");
-		Assert.assertEquals("CCC333", p.getPatenteSiguiente(703111));
+		patente = new Patente("BBB222");
+		Assert.assertEquals("CCC333", patente.getPatenteSiguiente(703111));
 	}
 
 	@Test
 	public void testPatente2016() {
-		Patente p;
+		Patente patente;
 
-		p = new Patente("AA001AA");
-		Assert.assertEquals("AA001BB", p.getPatenteSiguiente(27));
+		patente = new Patente("AA001AA");
+		Assert.assertEquals("AA001BB", patente.getPatenteSiguiente(27));
 
-		p = new Patente("AZ999ZZ");
-		Assert.assertEquals("BA000AA", p.getPatenteSiguiente(1));
+		patente = new Patente("AZ999ZZ");
+		Assert.assertEquals("BA000AA", patente.getPatenteSiguiente(1));
 
-		p = new Patente("BB222BB");
-		Assert.assertEquals("CC333CC", p.getPatenteSiguiente(18327063));
+		patente = new Patente("BB222BB");
+		Assert.assertEquals("CC333CC", patente.getPatenteSiguiente(18327063));
 	}
 }
