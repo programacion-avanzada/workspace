4a65adb6dc6eadaf35e8515e838688cf5e9735f5
Frederick <federicogasior@gmail.com>
2020-05-18 10:10:09 -0300

    Se sube ejemplo de Patrón: Chain of responsibility

---
+++ patrones/src/chainOfResponsibility/Default.java
@@ -0,0 +1,17 @@
+package chainOfResponsibility;
+
+public class Default implements Operacion {
+
+	public void establecerSiguiente(Operacion siguiente) {
+		throw new RuntimeException("No se puede agregar otro comportamiento");
+	}
+
+	public int calcular(Pedido pedido) {
+		throw new RuntimeException("\"" + pedido.getTipoDeCalculo() + "\" no es un tipo de operación válida\n");
+	}
+	
+	@Override
+	public String toString() {
+		return "Default";
+	}
+}
---
+++ patrones/src/chainOfResponsibility/Restar.java
@@ -0,0 +1,32 @@
+package chainOfResponsibility;
+
+public class Restar implements Operacion {
+
+	private Operacion siguiente;
+
+
+	public void establecerSiguiente(Operacion siguiente) {
+		this.siguiente = siguiente;
+	}
+
+	public int calcular(Pedido pedido) {
+
+		if (pedido.getTipoDeCalculo() == "restar") {
+			int resultado = pedido.getNumero1() - pedido.getNumero2();
+			System.out.print(pedido.getNumero1() + " - " + pedido.getNumero2());
+			System.out.print(" = " + resultado + "\n");
+
+			return resultado;
+		} else {
+			System.out.println("No puedo atenderlo, pasando al " + siguiente);
+			return siguiente.calcular(pedido);
+		}
+
+	}
+
+	@Override
+	public String toString() {
+		return "Restar";
+	}
+
+}
