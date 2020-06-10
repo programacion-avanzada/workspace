890a75f050d3737a1878ebb0af2905af8ccee6b3
Lucas Videla <videla.lucas@gmail.com>
2020-06-10 19:55:43 -0300

    Se remueve variable innecesaria

--- oia/armando-moviles/src/armadomoviles/Movil.java
+++ oia/armando-moviles/src/armadomoviles/Movil.java
@@ -3,12 +3,10 @@ package armadomoviles;
 public class Movil {
 	private int peso1;
 	private int peso2;
-	private final int varillas;
 
 	public Movil(int peso1, int peso2) {
 		this.peso1 = peso1;
 		this.peso2 = peso2;
-		this.varillas = 1;
 	}
 
 	public int getPesoTotal() {
