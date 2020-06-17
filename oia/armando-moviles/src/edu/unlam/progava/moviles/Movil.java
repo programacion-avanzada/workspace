6c6dc51f86c1a366e7fc839d85c5d658d769ee2d
hlanzillotta <66449120+hlanzillotta@users.noreply.github.com>
2020-06-16 21:20:24 -0300

    Ejercicio de OIA - Armando Móviles - (#7)
    * Ejercicio de OIA - Armando Móviles -
    
    Archivo comprimido que contiene el enunciado, el diagrama de clases y el proyecto de desarrollo en Java del ejercicio de la OIA "Armando Móviles"
    
    * Extracción y ordenamiento de oia-armando-moviles
    
    * Correcciones menores
    
    * Se remueve variable innecesaria
    
    * Cambios sutiles, agrega diagrama
    
    Co-authored-by: Frederick <federicogasior@gmail.com>
    Co-authored-by: Lucas Videla <videla.lucas@gmail.com>

---
+++ oia/armando-moviles/src/edu/unlam/progava/moviles/Movil.java
@@ -0,0 +1,15 @@
+package edu.unlam.progava.moviles;
+
+public class Movil {
+	private int peso1;
+	private int peso2;
+
+	public Movil(int peso1, int peso2) {
+		this.peso1 = peso1;
+		this.peso2 = peso2;
+	}
+
+	public int getPesoTotal() {
+		return this.peso1 + this.peso2;
+	}
+}
