270e1118a94fa898b48b8d6de7ac21baf7ba3baf
Lucas Videla <videla.lucas@gmail.com>
2021-05-20 10:46:12 -0300

    Revision sobre ejemplo de operaciones matematicas del Chain of Responsibility

--- patrones-chain-of-responsibility/src/edu/unlam/progava/chainofresponsibility/Restar.java
+++ patrones-chain-of-responsibility/src/edu/unlam/progava/chainofresponsibility/Restar.java
@@ -1,16 +1,9 @@
 package edu.unlam.progava.chainofresponsibility;
 
-public class Restar implements Operacion {
-
-	private Operacion siguiente;
-
-
-	public void establecerSiguiente(Operacion siguiente) {
-		this.siguiente = siguiente;
-	}
+public class Restar extends Operacion {
 
 	public int calcular(Pedido pedido) {
-
+		System.out.println("Intentando atender el pedido en Restar");
 		if (pedido.getTipoDeCalculo().equals("restar")) {
 			int resultado = pedido.getNumero1() - pedido.getNumero2();
 			System.out.print(pedido.getNumero1() + " - " + pedido.getNumero2());
